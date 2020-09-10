c729a743080a88a516a8b9cd0bfe7cc8a606df5f
miladezzat <miladezzat.f@gmail.com>
2020-09-10 22:42:27 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    count: 0,
+    time: null,
+  }
+  componentDidMount() {
+    setInterval(() => {
+      let time = new Date().toLocaleString();
+      this.setState({
+        time
+      })
+    }, 1000);
+  }
+  countUp = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+  render() {
+    return (
+      <div className='container'>        
+        <p className='timer'>{this.state.time}</p>
+      </div>
+    )
+  }
 }
-
-export default App;
